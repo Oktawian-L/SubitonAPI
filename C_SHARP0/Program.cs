65ee4321c7df7314b211c205ee5665cbd0809c8b
Oktawian-L <oktawianlasek@gmail.com>
2018-09-07 21:44:02 +0200

    foireach

--- C_SHARP0/Program.cs
+++ C_SHARP0/Program.cs
@@ -19,9 +19,61 @@ namespace C_SHARP0
                 Console.WriteLine(i);
             }
 
-            Console.ReadKey();
+            //OOP
+            var student = new Student();
+            student.Name = "Jimmy";
+            student.Phone = 6666666;
+            student.Birtday = "12-10-1990";
 
-            
+            var students = new List<Student>();
+
+            var adding = true;
+            while (adding)
+            {
+                var newStudent = new Student();
+                Console.Write("Dude name:");
+                newStudent.Name = Console.ReadLine();
+
+                Console.Write("dude grade:");
+                newStudent.Grade = int.Parse(Console.ReadLine());
+
+                students.Add(newStudent);
+                //wykorzystabie static z dolu
+                Student.Counter++;
+
+                Console.Write("Add another? y/n?");
+
+                if (Console.ReadLine() != "y")
+                    adding = false;
+
+
+            }
+
         }
+    }
+
+    class Student
+    {
+        //liczenie obi8ekjto
+        public static int Counter;
+
+        public string Name;
+        public int Grade;
+        public string Birtday;
+        public string Address;
+        int phone; //domyslnie leci priate
+        //c# property
+        //uzywasz tego jak wlasnosci klasy, przzypisujaca bezposnednio gdzeis wartosc
+        // student.Phone = 12321412;
+        public int Phone
+        {
+            set { phone = value;}
+        }
+
+        public void SetPhone(int number)
+        {
+            phone = number;
+        }
+            
     }
 }

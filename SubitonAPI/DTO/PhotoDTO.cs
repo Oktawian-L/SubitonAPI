64aa74adb5db7cea78b946875e81d4f7fd6dc7af
Oktawian-L <oktawianlasek@gmail.com>
2019-10-31 21:12:23 +0100

    Update PhotoDTO.cs

--- SubitonAPI/DTO/PhotoDTO.cs
+++ SubitonAPI/DTO/PhotoDTO.cs
@@ -15,5 +15,15 @@ namespace SubitonAPI.DTO
         public bool IsMain { get; set; }
         public int Set_user_nrid { get; set; }
     }
+    public class PhotoDTO3
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public int OrderNr { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public int Set_user_nrid { get; set; }
+    }
 }
 
